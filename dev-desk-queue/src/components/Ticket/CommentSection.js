625a1209f7fc3c4e79e1caf43de1ccbdcdf4659e
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-22 19:38:44 -0400

    final build

--- dev-desk-queue/src/components/Ticket/CommentSection.js
+++ dev-desk-queue/src/components/Ticket/CommentSection.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { UserInfo } from "./UserInfo";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { tomorrowNightEighties } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Container = styled.div`
   display: flex;
@@ -13,24 +15,52 @@ const CommentContainer = styled.div`
   flex-direction: column;
 `;
 
-const Message = styled.p`
+const Message = styled.div`
   font-size: 1.4rem;
   padding: 10px 30px;
 `;
 
 class CommentSection extends Component {
+  splitComment = comment => {
+    return comment.split("```");
+  };
+
   render() {
-    const {comments} = this.props;
+    const { comments } = this.props;
     return (
       <Container>
         {comments.map((comment, i) => {
           return (
             <CommentContainer key={i}>
-              <UserInfo openedBy={comment.opened_by} createdAt={comment.opened_by.created_at} bottom/>
-              <Message>{comment.comment}</Message>
+              <UserInfo
+                openedBy={comment.opened_by}
+                createdAt={comment.opened_by.created_at}
+                bottom
+              />
+              <Message style={{ whiteSpace: "pre-wrap" }}>
+                {this.splitComment(comment.comment).map((string, i) => {
+                  console.log('string',i, string);
+                  return i % 2 === 0 ? (
+                    string
+                  ) : (
+                    <SyntaxHighlighter
+                      language="text"
+                      style={tomorrowNightEighties}
+                      customStyle={{
+                        fontFamily: "Courier",
+                        width: "100%",
+                        fontSize: "1.2rem"
+                      }}
+                    >
+                      {string}
+                    </SyntaxHighlighter>
+                  );
+                })}
+              </Message>
+              {/* <Message style={{whiteSpace: "pre-wrap"}}>{comment.comment}</Message> */}
               {/* <button onClick={() => this.props.deleteComment(comment.id)}>Delete Comment</button> */}
             </CommentContainer>
-          )
+          );
         })}
       </Container>
     );
