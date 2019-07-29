f9c33c1c2f3096e8524201ab5b1db28e22fd584d
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-28 21:43:49 -0400

    new GlobalStyles component

---
+++ dev-desk-queue/src/styled-components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  @import url('https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,500,700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,700&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    background-color: #454f4f;
+  }  
+`;
+
+export default GlobalStyle;
