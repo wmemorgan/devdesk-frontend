5e01c9363296fa3517715229d4782642d1bf3dee
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-28 21:40:01 -0400

    added theme provider & global style component

--- dev-desk-queue/src/index.js
+++ dev-desk-queue/src/index.js
@@ -1,13 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router} from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styled-components/theme';
+import GlobalStyle from './styled-components/GlobalStyles';
+import App from './App';
 
 // implement withRouter HOC?
 
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>
-, document.getElementById("root"));
+  <Router>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Router>,
+  document.getElementById('root')
+);
