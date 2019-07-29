194705b90f2aafa62a45b2e8f90dd0e7d7b2a5a7
Dave Vazquez <dmvaz.li@gmail.com>
2019-07-28 22:40:58 -0400

    made top level component ThemeProvider

--- dev-desk-queue/src/App.js
+++ dev-desk-queue/src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
-import { Container } from "./styled-components/Dashboard_Styles";
-import { Route } from "react-router-dom";
-import { Auth } from "./authentication/Authentication";
-import LandingPage from "./components/LandingPage/LandingPage";
-import TicketForm from "./components/TicketForm/TicketForm";
-import TicketList from "./components/TicketList/TicketList";
-import Register from "./components/LandingPage/Register";
-import Ticket from "./components/Ticket/Ticket";
-import TopBar from "./components/TopBar/TopBar";
-import LogIn from "./components/LandingPage/LogIn";
-
+import React from 'react';
+import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
+import { Container } from './styled-components/TopBar_Styles';
+import { Route } from 'react-router-dom';
+import { Auth } from './authentication/Authentication';
+import LandingPage from './components/LandingPage/LandingPage';
+import TicketForm from './components/TicketForm/TicketForm';
+import TicketList from './components/TicketList/TicketList';
+import Register from './components/LandingPage/Register';
+import Ticket from './components/Ticket/Ticket';
+import TopBar from './components/TopBar/TopBar';
+import LogIn from './components/LandingPage/LogIn';
 
 class App extends React.Component {
   state = {
@@ -24,50 +23,50 @@ class App extends React.Component {
     });
   };
 
-  topBarHidden = (toggle) => {
+  topBarHidden = toggle => {
     this.setState({
       topBarHidden: toggle
-    })
-  }
+    });
+  };
 
   render() {
     const { activeUser } = this.state;
 
     return (
       <Container>
-        {Auth.isAuthenticated && <TopBar topBarHidden={this.topBarHidden}/>}
+        {Auth.isAuthenticated && <TopBar topBarHidden={this.topBarHidden} />}
         <Route
           exact
-          path="/"
+          path='/'
           render={props => (
             <LandingPage {...props} setActiveUser={this.setActiveUser} />
           )}
         />
         <Route
-          path="/login"
+          path='/login'
           render={props => (
             <LogIn {...props} setActiveUser={this.setActiveUser} />
           )}
         />
         <Route
-          path="/register"
+          path='/register'
           render={props => (
             <Register {...props} setActiveUser={this.setActiveUser} />
           )}
         />
         <PrivateRoute
           exact
-          path="/tickets"
+          path='/tickets'
           component={TicketList}
           activeUser={activeUser}
         />
         <PrivateRoute
-          path="/tickets/:id"
+          path='/tickets/:id'
           component={Ticket}
           activeUser={activeUser}
         />
         <PrivateRoute
-          path="/new-ticket"
+          path='/new-ticket'
           component={TicketForm}
           activeUser={activeUser}
         />
